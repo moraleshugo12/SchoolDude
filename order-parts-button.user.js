98abd18b739d3d1b11c30563335c87423c9a9b5b
moraleshugo12 <hugo.morales@dinuba.k12.ca.us>
2026-02-25 10:47:34 -0800

    Bump version from 1.6 to 1.7

--- order-parts-button.user.js
+++ order-parts-button.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Order Parts Button (Styled)
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  Adds an "Order Parts" button and a modern modal for part selection; handles Apps Script auth + autoclose.
 // @author       You
 // @match        *://*.schooldude.com/*
